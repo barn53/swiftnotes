9b596d5c75a4ccb024226f10d8f24ff1aba12da9
gerdmuller <gerdmuller.de@gmail.com>
2015-07-03 09:38:22 +0200

    Update theBeginning.swift

--- theBeginning.swift
+++ theBeginning.swift
@@ -5,46 +5,22 @@ enum IntervalModifier: String {
     case übermäßig  = "übermäßig"
     case vermindert = "vermindert"
     case tritonus   = "tritonus"
-    
+
     var name: String {
         return rawValue
     }
 }
 
 enum IntervalType: String {
-    case Prim    = "Prim"
-    case Second  = "Second"
-    case Terz    = "Terz"
-    case Quarte  = "Quarte"
-    case Quinte  = "Quinte"
-    case Sexte   = "Sexte"
-    case Septime = "Septe"
-    case Oktave  = "Oktave"
+    case Prim     = "Prim"
+    case Second   = "Second"
+    case Terz     = "Terz"
+    case Quarte   = "Quarte"
+    case Quinte   = "Quinte"
+    case Sexte    = "Sexte"
+    case Septime  = "Septe"
+    case Oktave   = "Oktave"
     
-    var halfTone : Int {
-        switch self {
-            case .Prim: return 0
-            case .Second: return 2
-            case .Terz: return 4
-            case .Quarte: return 5
-            case .Quinte: return 7
-            case .Sexte: return 9
-            case .Septime: return 11
-            case .Oktave: return 12
-        }
-    }
-    var line : Int {
-        switch self {
-            case .Prim: return 0
-            case .Second: return 1
-            case .Terz: return 2
-            case .Quarte: return 3
-            case .Quinte: return 4
-            case .Sexte: return 5
-            case .Septime: return 6
-            case .Oktave: return 7
-        }
-    }
     var name : String {
         return rawValue
     }
@@ -52,15 +28,15 @@ enum IntervalType: String {
 
 var matrix = [IntervalType : [IntervalModifier : (Int, Int?)]]()
 //                                                ^    ^
-//    Number of lines, number of halftones
-matrix[.Prim]    = [.rein: (0, 0  ), .groß: (0, nil), .klein: (0, nil), .übermäßig: (0, 1 ), .vermindert: (0, -1), .tritonus: (0, nil)]
-matrix[.Second]  = [.rein: (1, nil), .groß: (1, 2  ), .klein: (1, 1  ), .übermäßig: (1, 3 ), .vermindert: (1, 0 ), .tritonus: (1, nil)]
-matrix[.Terz]    = [.rein: (2, nil), .groß: (2, 4  ), .klein: (2, 3  ), .übermäßig: (2, 5 ), .vermindert: (2, 2 ), .tritonus: (2, nil)]
-matrix[.Quarte]  = [.rein: (3, 5  ), .groß: (3, nil), .klein: (3, nil), .übermäßig: (3, 6 ), .vermindert: (3, 4 ), .tritonus: (3, 6  )]
-matrix[.Quinte]  = [.rein: (4, 7  ), .groß: (4, nil), .klein: (4, nil), .übermäßig: (4, 8 ), .vermindert: (4, 6 ), .tritonus: (4, 6  )]
-matrix[.Sexte]   = [.rein: (5, nil), .groß: (5, 9  ), .klein: (5, 8  ), .übermäßig: (5, 10), .vermindert: (5, 7 ), .tritonus: (5, nil)]
-matrix[.Septime] = [.rein: (6, nil), .groß: (6, 11 ), .klein: (6, 10 ), .übermäßig: (6, 12), .vermindert: (6, 9 ), .tritonus: (6, nil)]
-matrix[.Oktave]  = [.rein: (7, 12 ), .groß: (7, nil), .klein: (7, nil), .übermäßig: (7, 13), .vermindert: (7, 11), .tritonus: (7, nil)]
+//    delta of lines, delta of halftones (nil means illegal modifier for interval)
+matrix[.Prim]     = [.rein: (0, 0  ), .groß: (0, nil), .klein: (0, nil), .übermäßig: (0, 1 ), .vermindert: (0, -1), .tritonus: (0, nil)]
+matrix[.Second]   = [.rein: (1, nil), .groß: (1, 2  ), .klein: (1, 1  ), .übermäßig: (1, 3 ), .vermindert: (1, 0 ), .tritonus: (1, nil)]
+matrix[.Terz]     = [.rein: (2, nil), .groß: (2, 4  ), .klein: (2, 3  ), .übermäßig: (2, 5 ), .vermindert: (2, 2 ), .tritonus: (2, nil)]
+matrix[.Quarte]   = [.rein: (3, 5  ), .groß: (3, nil), .klein: (3, nil), .übermäßig: (3, 6 ), .vermindert: (3, 4 ), .tritonus: (3, 6  )]
+matrix[.Quinte]   = [.rein: (4, 7  ), .groß: (4, nil), .klein: (4, nil), .übermäßig: (4, 8 ), .vermindert: (4, 6 ), .tritonus: (4, 6  )]
+matrix[.Sexte]    = [.rein: (5, nil), .groß: (5, 9  ), .klein: (5, 8  ), .übermäßig: (5, 10), .vermindert: (5, 7 ), .tritonus: (5, nil)]
+matrix[.Septime]  = [.rein: (6, nil), .groß: (6, 11 ), .klein: (6, 10 ), .übermäßig: (6, 12), .vermindert: (6, 9 ), .tritonus: (6, nil)]
+matrix[.Oktave]   = [.rein: (7, 12 ), .groß: (7, nil), .klein: (7, nil), .übermäßig: (7, 13), .vermindert: (7, 11), .tritonus: (7, nil)]
 
 struct Interval {
     var type = IntervalType.Prim
@@ -88,11 +64,11 @@ struct Interval {
     }
     
     var lines: Int {
-        return type.line
+        return matrix[type]![modifier]!.0
     }
     
     var halfTones: Int {
-        return 1
+        return matrix[type]![modifier]!.1!
     }
     
     var name: String {
@@ -101,9 +77,11 @@ struct Interval {
 }
 
 
-var terz = Interval(type: .Terz, modifier: .vermindert)
+var terz = Interval(type: .Terz, modifier: .groß)
 
-print("\(terz.name) -> valid: \(terz.checkTypeVsModifier())\n")
+print("\(terz.name) (\(terz.lines) , \(terz.halfTones)) -> valid: \(terz.checkTypeVsModifier()  )\n")
+
+
 
 
 
